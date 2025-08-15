ad58a3bd2c5ead96752eb11c276759540a1aff5c
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-08-15 12:16:46 +0800

    Create sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+// I assume this must be based on your actual domain, using env variables
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'http://localhost:3000',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'http://localhost:3000',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'http://localhost:3000/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}
