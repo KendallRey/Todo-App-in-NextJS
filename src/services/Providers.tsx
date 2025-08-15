deb4ef8797dd48a4825fb20f8db8e6183770d35b
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-08-15 12:15:46 +0800

    services - added Providers

---
+++ src/services/Providers.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React, { ReactNode } from 'react'
+import { QueryClientProvider } from '@tanstack/react-query';
+import { SnackbarProvider } from 'notistack';
+import { getQueryClient } from '../lib/query';
+import { TodoStoreProvider } from '@/stores/todo-store-provider';
+import { ThemeProvider } from 'next-themes'
+type ProvidersProps = {
+  children: ReactNode
+}
+
+const Providers = ({ children }: ProvidersProps) => {
+  const queryClient = getQueryClient()
+  return (
+    <>
+      <ThemeProvider disableTransitionOnChange>
+        <QueryClientProvider client={queryClient}>
+          <TodoStoreProvider>
+            <SnackbarProvider />
+            {children}
+          </TodoStoreProvider>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default Providers
