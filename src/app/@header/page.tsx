f74dd91b7ac23dcec80f8b3fd87d08d2f354e427
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-08-15 12:16:23 +0800

    parallel route - added HeaderPage

---
+++ src/app/@header/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+// I use MUI for this instance
+import { MuiToggleTheme } from '@/components'
+import React from 'react'
+
+const HeaderPage = () => {
+
+  return (
+    <div className='z-[10] w-full p-2 bg-blue-500 fixed top-0'>
+      <h1 className="text-3xl font-bold text-center">
+        ToDo List
+      </h1>
+      <div className='flex justify-end items-center gap-2'>
+        {/* <CustomToggleTheme /> */}
+        <MuiToggleTheme />
+      </div>
+    </div>
+  )
+}
+
+export default HeaderPage
